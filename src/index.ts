6a0dbe7460684a5d0ea84274f62c72beae7ed9a3
toluboy <tolualonge123@gmail.com>
2024-04-19 14:29:41 +0100

    stop cors error

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,16 @@ const app = express()
 dotenv.config()
 const port = 3000;
 
+// Define the CORS options
+
+app.use(cors());
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:3002', 'https://tailor-made-api-production.up.railway.app/']    
+    , // replace with your allowed origin
+    methods: ['GET', 'POST'], // specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // specify allowed headers
+  }));
 
 app.use(express.json({limit:"10mb"}));
 app.use(express.urlencoded({limit:"10mb", extended :true }))
